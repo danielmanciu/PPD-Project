5426a03f2df9214427af4f60b163145e1b6ebbf0
Daniel Manciu <daniel.manciu22@gmail.com>
2019-01-09 00:17:23 +0200

    Added multi-threaded support

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,61 @@
 import com.google.common.base.Stopwatch;
+import pixel.Rgb;
 import ppm.PpmFile;
-import processor.ImageProcessor;
 
 import java.io.File;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
     private static final String PPM_FILE_PATH = System.getProperty("user.dir") + File.separator + "image.ppm";
     private static final String NEW_PPM_FILE_PATH = System.getProperty("user.dir") + File.separator + "processed-image.ppm";
 
+    private static PpmFile processPpmFile(final PpmFile ppmFile, final ExecutorService executor) {
+        String format = ppmFile.getFormat();
+        int width = ppmFile.getWidth();
+        int height = ppmFile.getHeight();
+        int maximumValue = ppmFile.getMaximumValue();
+        Rgb[][] pixels = ppmFile.getRgbPixels();
+
+        for (int row = 0; row < height; row++) {
+            final int i = row;
+            executor.submit(() -> {
+                for (int j = 0; j < width; j++) {
+                    pixels[i][j].applySepia();
+                }
+            });
+        }
+
+        return new PpmFile(format, width, height, maximumValue, pixels);
+    }
+
     public static void main(String[] args) {
+        final ExecutorService executor = Executors.newFixedThreadPool(4);
+
         Stopwatch stopwatch = Stopwatch.createStarted();
 
-        PpmFile ppmFile = new PpmFile(PPM_FILE_PATH);
+        final PpmFile ppmFile = new PpmFile(PPM_FILE_PATH);
         System.out.println("Read time: " + stopwatch.stop().elapsed(TimeUnit.MILLISECONDS) + " ms");
 
         stopwatch = Stopwatch.createStarted();
 
-        PpmFile processedPpmFile = ImageProcessor.process(ppmFile);
+        final PpmFile processedPpmFile = processPpmFile(ppmFile, executor);
         System.out.println("Process time: " + stopwatch.stop().elapsed(TimeUnit.MILLISECONDS) + " ms");
 
         stopwatch = Stopwatch.createStarted();
 
         processedPpmFile.writeToFile(NEW_PPM_FILE_PATH);
         System.out.println("Write time: " + stopwatch.stop().elapsed(TimeUnit.MILLISECONDS) + " ms");
+
+        try {
+            executor.awaitTermination(1, TimeUnit.SECONDS);
+            executor.shutdown();
+        }
+        catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
--- src/pixel/Rgb.java
+++ src/pixel/Rgb.java
@@ -5,17 +5,19 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.io.Serializable;
+
 @AllArgsConstructor
 @Getter
 @Setter
 @ToString
-public class Rgb {
+public class Rgb implements Serializable {
 
     private int R;
     private int G;
     private int B;
 
-    public void applySepia() {
+    public Rgb applySepia() {
         final int newR = clamp((int) ((R * 0.393) + (G * 0.769) + (B * 0.189)));
         final int newG = clamp((int) ((R * 0.349) + (G * 0.686) + (B * 0.168)));
         final int newB = clamp((int) ((R * 0.272) + (G * 0.534) + (B * 0.131)));
@@ -23,6 +25,8 @@ public class Rgb {
         R = newR;
         G = newG;
         B = newB;
+
+        return new Rgb(R, G, B);
     }
 
     private static int clamp(int val) {
